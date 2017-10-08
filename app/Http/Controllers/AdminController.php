2061287d186e317a6383939a56d2977e4ce62c89
Yonatan Nugraha <yonatannugraha@Yonatans-MacBook-Air.local>
2017-10-08 17:15:29 +0700

    tes staging deploy

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -29,7 +29,7 @@ class AdminController extends Controller
      */
     public function index()
     {
-        dd('a');
+        dd('a'); 
         $today      = Carbon::now();
         $last_month = Carbon::now()->subDays(30);
 
