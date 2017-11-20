6cadc5423ec22e518a7e40a71ddeaa912182a083
yonatan.nugraha <yonatan.nugraha@hotmail.com>
2017-11-20 19:17:46 +0700

    move product images

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -41,22 +41,7 @@ class Product extends Model
      */
     public function image()
     {
-        $image_name = $this->image_name;
-        $category   = $this->category;
-
-        if ($category == 'Pulsa') {
-            $folder_name = 'pulsa';
-        } else if ($category == 'PDAM') {
-            $folder_name = 'pdam';
-        } else if ($category == 'TV Kabel') {
-            $folder_name = 'tv';
-        } else if ($category == 'Angsuran Kredit') {
-            $folder_name = 'finance';
-        } else {
-            return null;
-        }
-
-        $file_path =  "/images/products/$folder_name/$image_name";
+        $file_path =  '/images/products/' . $this->image_name;
         if (file_exists(public_path() . $file_path)) {
             return $file_path;
         }
